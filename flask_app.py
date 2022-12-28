e99bc6a8bc83512c5b86c7d65231f90032a63251
Jim Thompson <jimthompson5802@gmail.com>
2022-12-28 11:41:16 -0500

    updated flask example

---
+++ flask_app.py
@@ -0,0 +1,64 @@
+# import Flask class from flask module
+from flask import Flask, render_template, request
+
+# function to compute value of two numbers based on operator flag
+def compute_value(num1, num2, operator):
+    if operator == "add":
+        return num1 + num2
+    elif operator == "subtract":
+        return num1 - num2
+    elif operator == "multiply":
+        return num1 * num2
+    elif operator == "divide":
+        return num1 / num2
+    else:
+        return "Invalid operator"
+
+
+# define string for compute_value web page form
+compute_value_form = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Compute Value</title>
+</head>
+<body>
+<h1>Compute Value</h1>
+<form action="/compute" method="post">
+    <label for="num1">Number 1:</label>
+    <input type="text" id="num1" name="num1"><br><br>
+    <label for="num2">Number 2:</label>
+    <input type="text" id="num2" name="num2"><br><br>
+    <label for="operator">Operator:</label>
+    <select id="operator" name="operator">
+        <option value="add">Add</option>
+        <option value="subtract">Subtract</option>
+        <option value="multiply">Multiply</option>
+        <option value="divide">Divide</option>
+    </select><br><br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
+"""
+
+
+# setup flask app to accept connection from any source on port 8080
+# to server compute_value web page form
+app = Flask(__name__)
+@app.route('/compute', methods=['GET', 'POST'])
+def compute():
+    if request.method == 'POST':
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        operator = request.form['operator']
+        return str(compute_value(num1, num2, operator))
+    else:
+        return compute_value_form
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+
