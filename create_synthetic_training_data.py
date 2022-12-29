f6bcaaeb130dc25a15fc822b231f5e46ef1491f2
Jim Thompson <jimthompson5802@gmail.com>
2022-12-29 10:48:38 -0500

    add program to create synthetic data

---
+++ create_synthetic_training_data.py
@@ -0,0 +1,17 @@
+
+
+# create a synthetic regression dataset with 5 features and 1000  samples and create a pandas dataframe
+from sklearn.datasets import make_regression
+import pandas as pd
+import numpy as np
+
+# create a synthetic regression dataset with 5 features and 1000  samples and create a pandas dataframe
+X, y = make_regression(n_samples=1000, n_features=5, n_informative=5, random_state=1)
+df = pd.DataFrame(X, columns=['input_feat_1', 'input_feat_2', 'input_feat_3', 'input_feat_4', 'input_feat_5'])
+df['target'] = y
+
+# print first 5 rows of dataframe
+print(df.head())   # Note: added print statement
+
+# save dataframe to csv file
+df.to_csv('data.csv', index=False)
