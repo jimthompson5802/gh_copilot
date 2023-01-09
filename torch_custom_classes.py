5165057cf79f67f9af9d368ce2b14b2936ac7677
Jim Thompson <jimthompson5802@gmail.com>
2023-01-09 18:40:31 -0500

    add another example

--- torch_custom_classes.py
+++ torch_custom_classes.py
@@ -38,6 +38,34 @@ class MyClass(nn.Module):
         return x
 
 
+# custom pytorch model class with convolutional layer for 3 channels followed by max pool followed by 3 layers 128, 64, 10 nodes
+# with ReLU activation and log softmax output
+class CustomModel(nn.Module):
+    def __init__(self):
+        super(CustomModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1)
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, 10)
+        self.max_pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.max_pool(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+
 # class for custom pytorch dataset
 class CustomDataset(Dataset):
     def __init__(self, data, target):
