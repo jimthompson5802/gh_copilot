cb2a2f04be9645c7e9d1db9a9da5d1570d9bf8c5
Jim Thompson <jimthompson5802@gmail.com>
2023-01-12 20:26:24 -0500

    add simple unit test examples

--- sample_java_code.java
+++ sample_java_code.java
@@ -16,3 +16,29 @@ ResultSet rs = stmt.executeQuery("SELECT * FROM table");
 while (rs.next()) {
     System.out.println(rs.getString("column"));
 }
+
+// function to compute value of two numbers based on an operator  Commentary: this was typed by hand
+public static int compute(int a, int b, String operator) {
+    if (operator.equals("+")) {
+        return a + b;
+    } else if (operator.equals("-")) {
+        return a - b;
+    } else if (operator.equals("*")) {
+        return a * b;
+    } else if (operator.equals("/")) {
+        return a / b;
+    } else {
+        return 0;
+    }
+}
+
+// junit test case for compute function  Commentary:  this was typed by hand
+@Test
+public void testCompute() {
+    assertEquals(3, compute(1, 2, "+"));
+    assertEquals(-1, compute(1, 2, "-"));
+    assertEquals(2, compute(1, 2, "*"));
+    assertEquals(0, compute(1, 2, "/"));
+    assertEquals(0, compute(1, 2, "a"));
+}
+
