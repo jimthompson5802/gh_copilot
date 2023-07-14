225026b659246cdd142ac30ca0b3fa6e7d758b06
Jim Thompson <jimthompson5802@gmail.com>
2023-07-14 13:56:53 -0400

    updated example with sas macro use case

--- codex/translate_sas_to_python.py
+++ codex/translate_sas_to_python.py
@@ -18,7 +18,7 @@ def translate_sas_to_python(sas_code):
         engine='text-davinci-003',
         prompt=prompt,
         max_tokens=2048,
-        temperature=0.6,
+        temperature=0.0,
         n=1,
         stop=None,
     )
@@ -52,13 +52,16 @@ proc print data=model_diagnostics;
 run;
 '''
 
-# Translate SAS code to Python
+###
+# Example SAS code to translate
+###
+
 python_code = translate_sas_to_python(sas_code)
+print("\n### generated simple SAS Code ###")
 print(python_code)
 
 
-### generated output ###
-# # Import the necessary packages
+### generated simple SAS Code ###
 # import pandas as pd
 # from sklearn.linear_model import LinearRegression
 #
@@ -68,10 +71,59 @@ print(python_code)
 # # Generate the linear regression model
 # X = mydata[['x1', 'x2', 'x3']]
 # y = mydata['y']
-# model = LinearRegression()
-# model.fit(X, y)
+# model = LinearRegression().fit(X, y)
 #
 # # Print model diagnostics
-# print('Model intercept:', model.intercept_)
-# print('Model coefficients:', model.coef_)
-# print('Model R-squared:', model.score(X, y))
+# model_diagnostics = pd.DataFrame({'p': model.pvalues_, 'pchi': model.pvalues_, 'r': model.score(X, y), 'rstudent': model.score(X, y)})
+# print(model_diagnostics)
+
+
+####
+# Example SAS macro code to translate
+###
+
+sas_macro_code = '''
+%macro generate_regression_models(input_files);
+  %local i;
+
+  %do i = 1 %to %sysfunc(countw(&input_files));
+    %let input_file = %scan(&input_files, &i);
+
+    data mydata;
+      infile "&input_file" dlm=',' firstobs=2;
+      input x y;
+    run;
+
+    proc reg data=mydata outest=outest&i;
+      model y = x;
+    run;
+
+    %put Linear regression model for &input_file has been generated;
+  %end;
+
+  %put All regression models have been generated successfully;
+%mend;
+
+%generate_regression_models(input_files = "path/to/file1.csv path/to/file2.csv path/to/file3.csv");
+'''
+
+# Translate SAS code to Python
+python_macro_code = translate_sas_to_python(sas_macro_code)
+print("\n### generated SAS Macro Code ###")
+print(python_macro_code)
+
+### generated SAS Macro Code ###
+# from sklearn.linear_model import LinearRegression
+# import pandas as pd
+#
+# input_files = ["path/to/file1.csv", "path/to/file2.csv", "path/to/file3.csv"]
+#
+# for i in range(len(input_files)):
+#   input_file = input_files[i]
+#   df = pd.read_csv(input_file, delimiter=',', skiprows=1)
+#   X = df['x'].values.reshape(-1,1)
+#   y = df['y'].values.reshape(-1,1)
+#   model = LinearRegression().fit(X, y)
+#   print("Linear regression model for {} has been generated".format(input_file))
+#
+# print("All regression models have been generated successfully")
