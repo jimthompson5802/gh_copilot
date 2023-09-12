2679416441cf9a69af8cc9b222e504403062b894
vinodrjv <vinodrjv@gmail.com>
2023-09-12 02:03:10 +0000

    Removed the archive directory

--- codex/documentation-to-code/BostonHomePricing/archive/20230909_001742/workspace/run.sh
+++
@@ -1,5 +0,0 @@
-python3 -m venv env
-source env/bin/activate
-pip install pandas numpy scikit-learn joblib
-
-python housing_regression.py
--- codex/documentation-to-code/BostonHomePricing/archive/20230911_222103/workspace/run.sh
+++
@@ -1,5 +0,0 @@
-# a) Install dependencies
-pip install -r requirements.txt
-
-# b) Run all necessary parts of the codebase
-python main.py
